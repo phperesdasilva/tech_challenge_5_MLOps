4909b11a4a6a8e54de559a7eb3e2313962fb72d7
phperesdasilva <phperesdasilva@gmail.com>
2026-06-08 18:58:12 -0300

    getting data from kaggle

---
+++ data/kaggle_db/scripts/kaggle_variables.py
@@ -0,0 +1,7 @@
+USERNAME = 'phperesdasilva'
+API_TOKEN = 'KGAT_7ecdab89b3110697cb4307d994ab2dc2'
+
+DATASET1 = 'aguado/telemarketing-jyb-dataset'
+DATASET2 = 'tunguz/bank-marketing-data-set'
+DATASET3 = 'henriqueyamahata/bank-marketing' 
+DATASET4 = 'dharmik34/bank-term-deposit-subscription'
