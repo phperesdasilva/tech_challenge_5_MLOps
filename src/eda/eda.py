af872b7914be0225d4ac1ba3839874bf0c94b7b3
phperesdasilva <phperesdasilva@gmail.com>
2026-06-18 20:15:26 -0300

    get-raw-data calling eda.py main

---
+++ src/eda/eda.py
@@ -0,0 +1,8 @@
+from eda.DataManager import DataManager
+
+
+def main():
+    print("🚀 Iniciando processo de EDA...")
+    data_manager = DataManager()
+    data_manager.get_raw_data()
+    print("✅ EDA concluída com sucesso!")
