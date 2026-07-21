76b742fd2f62aadda2f07d1d8a0ecdb41e0e2d80
phperesdasilva <phperesdasilva@gmail.com>
2026-07-21 20:12:21 -0300

    llm main loop

--- src/llm/llm_main.py
+++ src/llm/llm_main.py
@@ -4,20 +4,21 @@ app = build_graph()
 
 def run_llm(prompt=None):
 
-    if prompt is None:
+    while True:
+
         prompt = input("Digite seu prompt (ou 'sair' para encerrar): ")
-        print(f"\nPrompt enviado para o modelo...\n")
-    if prompt.lower() == 'sair':
-        print("Encerrando a interação com o modelo.")
-        return
+
+        if prompt.lower() == 'sair':
+            print("Encerrando a interação com o modelo.")
+            break
 
 
-    output = app.invoke({"prompt": prompt})
+        output = app.invoke({"prompt": prompt})
 
-    print(f"""
-⬜⬜⬜⬜⬜⬜⬜
+        print(f"""
+    ⬜⬜⬜⬜⬜⬜⬜
 
-{output['output']}
+    {output['output']}
 
-⬜⬜⬜⬜⬜⬜⬜
-""")
+    ⬜⬜⬜⬜⬜⬜⬜
+    """)
